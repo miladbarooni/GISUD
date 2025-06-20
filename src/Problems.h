2aa7989041de40b8ec9af391a4324829315aadbb
Frederic Quesnel <frederic.quesnel@gerad.ca>
2025-06-20 13:50:26 -0400

    removed some unused methods from Problems class

--- src/Problems.h
+++ src/Problems.h
@@ -8,21 +8,6 @@
 #include "CplexMIP.h"
 #include "ISUD.h"
 
-// Construct ISUD_Base from paths
-// "mps_file" is the mps_file path
-// Compute initial solution for problem "problem"
-// "solution" is the optimal solution of problem "problem"
-// "perturbation_percent" is the percentage of remaining columns in new initial solution
-// Out dir is the output directory where to put new problems with new initial solutions
-int computeInitialSolution(std::vector<int> solution, double perturbation_percent, ISUD_Base& problem, std::string out_dir);
-
-// Export problem to a file in the GISUD format
-// "problem" is the problem
-// "columns_file" is the path to the columns file
-// "rhs_file" is the path to the rhs file
-// "initial_solution_file" is the path of the initial solution file
-// "fixed_cost_path" is the path of the fixed cost file
-void export_problem(ISUD_Base* problem, std::string columns_file, std::string rhs_file, std::string initial_solution_file, std::string fixed_cost_file = "");
 
 // Construct ISUD_Base from paths
 // "mps_file" is the mps_file path
